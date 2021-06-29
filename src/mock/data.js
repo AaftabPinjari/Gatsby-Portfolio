f99b886bfc6be105cf534aed4f86e68ca2f457c8
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-06-29 15:15:10 +0530

    added the projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,118 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Aaftab | Portfolio',
+  lang: 'en',
+  description: 'Welcome to my Portfolio',
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Aaftab Pinjari',
+  subtitle: 'I\'m a Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'aaftab.jpg',
+  paragraphOne: 'I am a Passionate Web Developer Looking for Oppurtunities to apply my skills in the real world Applications.',
+  paragraphTwo: 'My Skills Include HTML,CSS ,JavaScript ,ReactJs ,GatsbyJs ,NextJs Python ,Bootstrap ,Tailwind ,NodeJs ,ExpressJs.',
+  paragraphThree: 'Checkout My Projects to get to Know More.',
+  // resume: 'https://www.resumemaker.online/es.php', 
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '01.jpg',
+    title: 'Hear Us',
+    info: 'An Internship Project to make the Website of Company Responsive and SEO Friendly.',
+    info2: 'Made with Technologies like NextJs , TaiwindCSS and With The Functionality of MDX Blog.',
+    url: 'https://hear-us.vercel.app/',
+    repo: 'https://github.com/AaftabPinjari/hear-us',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '02.jpg',
+    title: 'React E-Commerce',
+    info: 'A Full Stack E-commerce Personal Project Availing Backend API Calls.  ',
+    info2: 'Made With Technologies Like ReactJs , Commercejs, Material UI and Stripe Payment API',
+    url: 'https://mesh-agro.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/React-E-Commerce',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '03.jpg',
+    title: 'Gatsby MDX Blog',
+    info: 'A Personal Blog Website that is Responsive and Fast Every way that matters.',
+    info2: 'Made with Technologies like Gatsbyjs , Tailwind and with MDX Blog Functionality.',
+    url: 'https://aaftabpinjari.gatsbyjs.io/',
+    repo: 'https://github.com/AaftabPinjari/my-portfolio',
+  },
+  {
+    id: nanoid(),
+    img: '04.jpg',
+    title: 'Jobs By Github',
+    info: 'Website that is Responsive and Fast Every way that matters.',
+    info2: 'Made with Technologies like Reactjs , Bootstrap and Github Jobs API.',
+    url: 'https://aaftabs-github-jobs.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/github-jobs-api',
+  },
+  {
+    id: nanoid(),
+    img: '05.jpg',
+    title: 'Recipe App',
+    info: 'Website that is Responsive and Fast Every way that matters.',
+    info2: 'Made with Technologies like Reactjs and Recipe API.',
+    url: 'https://aaftabs-menu-app.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/food-menu-app',
+  },
+  {
+    id: nanoid(),
+    img: '06.jpg',
+    title: 'Travel Mentor',
+    info: 'Website that is Responsive and Fast Every way that matters.',
+    info2: 'Made with Technologies like Reactjs and Travel API.',
+    url: 'https://aaftabs-tours-and-travels.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/Tours-and-travels',
+  },
+  {
+    id: nanoid(),
+    img: '07.jpg',
+    title: 'Shades And Tints',
+    info: 'A Website To make Picking Color Pallete Easy and Efficient.',
+    info2: 'Made with Technologies like Reactjs with Valuejs JavaScript Library.',
+    url: 'https://shades-and-tints.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/color-shades-and-tints',
+  },
+  {
+    id: nanoid(),
+    img: '08.jpg',
+    title: 'Shower Thoughts',
+    info: 'A Website for you to shout out loud your Shower Thoughts.',
+    info2: 'Made with Technologies like Reactjs with CRUD Functionality.',
+    url: 'https://shower-thought.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/shower-thought',
+  },
+  {
+    id: nanoid(),
+    img: '09.jpg',
+    title: 'BDay Memo',
+    info: 'A Website to remember ypur friends birthdays.',
+    info2: 'Made with Technologies like Reactjs with Edit Functionality.',
+    url: 'https://aaftabs-birthday-memo.netlify.app/',
+    repo: 'https://github.com/AaftabPinjari/Birthday-Memo',
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Email',
+  email: 'pinjari.aaftab1@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +122,24 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/AaftabPinjari4',
     },
     {
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/Aaftab786',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/aaftab-pinjari-2aba4b168/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/AaftabPinjari',
     },
   ],
 };
 
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
-};
+
